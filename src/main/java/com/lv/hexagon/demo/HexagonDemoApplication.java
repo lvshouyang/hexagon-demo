ca5a926547f4456c311d61dfb7f3df7080e32707
shouyang.lv <shouyang.lv@accentrue.com>
2022-07-20 14:37:53 +0800

    init hexagon demo

---
+++ src/main/java/com/lv/hexagon/demo/HexagonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lv.hexagon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lv/hexagon/demo/HexagonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv.hexagon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
